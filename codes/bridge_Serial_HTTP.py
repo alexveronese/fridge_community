b91c75e0f4b77b7cc9b57ed4c85c38b281bd94e4
Bea <calderara.beatrice@gmail.com>
2024-12-08 19:22:03 +0100

    Added arduino_id functionality.

--- codes/bridge_Serial_HTTP.py
+++ codes/bridge_Serial_HTTP.py
@@ -1,6 +1,5 @@
-
 import serial
-import serial.tools.list_ports
+from serial import tools
 import requests
 import time
 import configparser
@@ -8,106 +7,114 @@ import configparser
 
 class Bridge():
 
-	def __init__(self):
-		self.config = configparser.ConfigParser()
-		self.config.read('config.ini')
-		#self.postdata(0, 2)  prova post
-		self.setupSerial()
+    def __init__(self):
+        self.config = configparser.ConfigParser()
+        self.config.read('config.ini')
+        # self.postdata(0, 2)  prova post
+        self.setupSerial()
 
-	def postdata(self, i, val):
-		if i > 0:
-			return
-		url = self.config.get("DJANGO","Url") + "/data"
-		myobj = {'value': val}
-		headers = {'X-AIO-Key': self.config.get("DJANGO","X-AIO-Key") }
-		print ("> Sending to " + url)
+    def postdata(self, i, val):
+        if i > 0:
+            return
+        url = self.config.get("DJANGO", "Url") + "/data"
+        myobj = {'value': val}
+        headers = {'X-AIO-Key': self.config.get("DJANGO", "X-AIO-Key")}
+        print("> Sending to " + url)
 
-		x = requests.post(url, data=myobj, headers=headers)
-		print(x.json())
+        x = requests.post(url, data=myobj, headers=headers)
+        print(x.json())
 
-	def setupSerial(self):
-		# open serial port
-		self.ser = None
+    def setupSerial(self):
+        # open serial port
+        self.ser = None
 
-		if not self.config.getboolean("Serial","UseDescription", fallback=False):
-			self.portname = self.config.get("Serial","PortName", fallback="COM1")
-		else:
-			print("list of available ports: ")
-			ports = serial.tools.list_ports.comports()
+        if not self.config.getboolean("Serial", "UseDescription", fallback=False):
+            self.portname = self.config.get("Serial", "PortName", fallback="COM1")
+        else:
+            print("list of available ports: ")
+            ports = serial.tools.list_ports.comports()
 
-			for port in ports:
-				print (port.device)
-				print (port.description)
-				if self.config.get("Serial","PortDescription", fallback="arduino").lower() \
-						in port.description.lower():
-					self.portname = port.device
+            for port in ports:
+                print(port.device)
+                print(port.description)
+                if self.config.get("Serial", "PortDescription", fallback="arduino").lower() \
+                        in port.description.lower():
+                    self.portname = port.device
 
-		try:
-			if self.portname is not None:
-				print ("connecting to " + self.portname)
-				self.ser = serial.Serial(self.portname, 9600, timeout=0)
-				print("CONNESSO alla porta")
-		except:
-			print("ECCEZIONE")
-			self.ser = None
+        try:
+            if self.portname is not None:
+                print("connecting to " + self.portname)
+                self.ser = serial.Serial(self.portname, 9600, timeout=0)
+                print("CONNESSO alla porta")
+        except:
+            print("ECCEZIONE")
+            self.ser = None
 
-		# self.ser.open()
+        # self.ser.open()
 
-		# internal input buffer from serial
-		self.inbuffer = []
+        # internal input buffer from serial
+        self.inbuffer = []
 
+    def loop(self):
+        lasttime = time.time()
+        connect = 0
+        while (True):
+            current_time = time.time()
+            # look for a byte from serial
+            if not self.ser is None:
+                while self.ser.inWaiting() != 0:
 
-	def loop(self):
-		lasttime = time.time()
-		connect = 0
-		while(True):
-				current_time = time.time()
-				#look for a byte from serial
-				if not self.ser is None:
-					while self.ser.inWaiting() != 0:
+                    # data available from the serial port
+                    lastchar = self.ser.read(1)
 
-						# data available from the serial port
-						lastchar = self.ser.read(1)
+                    # se viene inviato il carattere di inizio
+                    if connect == 0 and lastchar == b'\xff':
+                        print("Connesso, leggo i dati...")
+                        connect = 1
+                        continue
 
-						#se viene inviato il carattere di inizio
-						if connect == 0 and lastchar == b'\xff':
-							print("Connesso, leggo i dati...")
-							connect = 1
-							continue
+                    if connect == 1:
+                        if lastchar == b'\xfe':  # EOL
+                            connect = 0
+                            print("\nValue received")
+                            print(self.inbuffer)
 
-						if connect == 1:
-							if lastchar == b'\xfe': #EOL
-								connect = 0
-								print("\nValue received")
-								print(self.inbuffer)
-								self.useData()
-								self.inbuffer = []
-							else:
-								self.inbuffer.append(lastchar)
+                            # check if there's an ID and enough data
+                            if len(self.inbuffer) < 3:  # ID + at least 1 data + footer
+                                print("Packet is too short, ignored")
+                            else:
+                                self.useData()  # process data
 
-				# If no serial data, still check if 2 seconds have passed
-				elif current_time - lasttime >= 2:
-					print("no data")
-					break
+                            # clear the buffer
+                            self.inbuffer = []
 
+                        else:
+                            self.inbuffer.append(lastchar)
 
+            # If no serial data, still check if 2 seconds have passed
+            elif current_time - lasttime >= 2:
+                print("no data")
+                break
 
-	def useData(self):
-		# I have received a packet from the serial port. I can use it
-		if len(self.inbuffer)<2:   # at least header, size, footer
-			print("inbuffer short")
-			return False
-		# split parts
+    def useData(self):
+        # I have received a packet from the serial port. I can use it
+        if len(self.inbuffer) < 3:  # at least header, id, size?, footer
+            print("inbuffer short")
+            return False
 
-		#numval = int.from_bytes(self.inbuffer[1], byteorder='little')
-		#print(numval)
+        # extract arduino ID
+        arduino_id = int.from_bytes(self.inbuffer[1], byteorder='little')
+        print("Arduino ID received: ", arduino_id, ", data: ", self.inbuffer[1:6])
 
-		for i in range(2):
-			val = int.from_bytes(self.inbuffer[i], byteorder='little')
-			print(val)
-			#self.postData(i, val)
+        print("\nPrinting: {")
+        #read all sent values (also Arduino ID)
+        for i in range(len(self.inbuffer)):
+            val = int.from_bytes(self.inbuffer[i], byteorder='little')
+            #self.postData(i, val)
+            print(val, ",")
+        print("}")
+
 
 if __name__ == '__main__':
-	br = Bridge()
-	br.loop()
-
+    br = Bridge()
+    br.loop()
