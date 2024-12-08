c3a5d8898e88fb68e5f52c96c7c3071cf8ebd9da
alexveronese <302474@studenti.unimore.it>
2024-12-08 13:36:14 +0100

    add models

--- config/main/models.py
+++ config/main/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.db.models import ForeignKey
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Fridge(models.Model):
+    serial_number = models.CharField(max_length=10, unique=True)
+    secret_number = models.CharField(max_length=3)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+
+class SensorFeed(models.Model):
+    fridge = ForeignKey(Fridge, on_delete=models.CASCADE)
+
+    door = models.BooleanField(default=False)
+    int_temp = models.IntegerField(default=0)
+    int_hum = models.IntegerField(default=0)
+    ext_temp = models.IntegerField(default=0)
+    ext_hum = models.IntegerField(default=0)
+    power_consumption = models.IntegerField(default=0)
+
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+"""
+class Anomaly(models.Model):
+    fridge = ForeignKey(Fridge, on_delete=models.CASCADE)
+"""
