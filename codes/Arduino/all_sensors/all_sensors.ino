afdcc62e3410f4e3eacea6ea8bdd09bebfb1c65b
Fede Lopre <lpfederico40@gmail.com>
2024-12-09 11:54:54 +0100

    creato allarme temp e apertura frigo fixed #2

--- codes/Arduino/all_sensors/all_sensors.ino
+++ codes/Arduino/all_sensors/all_sensors.ino
@@ -4,6 +4,9 @@ SimpleDHT11 dht11;       //instantiates the SimpleDHT11 Object class to variable
 const int buttonPin = 2;   //0 = fridge closed; 1 = fridge open
 const int pinDHT11int = 5;
 const int pinDHT11ext = 6;
+const int BUZZER_PIN = 3;
+const int MAX_TEMP = 7;
+
 
 int buttonState = 0;              // open/close
 float temperatureIn = 0.0;        // internal temperature and humidity
@@ -15,26 +18,29 @@ float humidityOut = 0.0;
 int conv_temperatureOut = 0;      // values casted to int
 int conv_humidityOut = 0;
 int potentiometerValue = 0;       // power consumption
+byte temp_alarm = 0;
 
 unsigned long timestamp;
+unsigned long openFridgeTime;
 
 const byte arduinoID = 0x01;      // unique ID Arduino: change this for each device
 
 void setup() {
   Serial.begin(9600);
   timestamp = millis();
+  openFridgeTime = millis();
   pinMode(buttonPin, INPUT);
+  pinMode(BUZZER_PIN, OUTPUT);
 }
 
 void loop() {
-  if(millis() - timestamp > 5000){      //delay
     //read the button value
     buttonState = digitalRead(buttonPin);
 
-    //read the potentiometer value 
+    //read the potentiometer value
     potentiometerValue = analogRead(A0);
     potentiometerValue = map(potentiometerValue, 0, 1023, 0, 253);
-    
+
     //read internal temperature and humidity values
     byte dataInt[40] = {0};
     if (dht11.read2(pinDHT11int, &temperatureIn, &humidityIn, dataInt)) {
@@ -48,7 +54,24 @@ void loop() {
       Serial.print("Read DHT11 failed");
       return;
     }
-    
+    // check data
+    if(temperatureIn > MAX_TEMP){
+        temp_alarm = 1;
+    }
+
+    if(buttonState == 0){
+        openFridgeTime = millis();
+    } else if(millis() - openFridgeTime > 10000){
+        // until closed
+        while(digitalRead(buttonPin)){
+            digitalWrite(BUZZER_PIN, HIGH);
+        }
+        digitalWrite(BUZZER_PIN,LOW);
+        openFridgeTime = millis();
+    }
+    //se passa abbastanza tempo invio i dati
+  if(millis() - timestamp > 5000){      //delay
+
     //just for debugging
     //Serial.println(temperatureIn);
     //Serial.println(humidityIn);
@@ -68,7 +91,6 @@ void loop() {
     //Serial.println(temperatureOut); Serial.print("°C, ");
     //Serial.println(humidityOut); Serial.println("%");
 
-    
     Serial.write(0xFF);              //255: init char
     Serial.write(arduinoID);         //ID Arduino 
     Serial.write(buttonState);
@@ -77,6 +99,7 @@ void loop() {
     Serial.write(conv_temperatureOut);
     //Serial.write(conv_humidityOut);
     Serial.write(potentiometerValue);
+    Serial.write(temp_alarm);
     Serial.write(0xFE);               //254: end char
   
     timestamp = millis();
