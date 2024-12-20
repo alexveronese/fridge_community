94c274fe02837897c3d3b6e0851de8f709b9d047
alexveronese <302474@studenti.unimore.it>
2024-12-20 15:38:23 +0100

    formatted arduino code

--- codes/Arduino/all_sensors/all_sensors.ino
+++ codes/Arduino/all_sensors/all_sensors.ino
@@ -1,8 +1,8 @@
 #include <SimpleDHT.h>
 
-SimpleDHT11 dht11;       //instantiates the SimpleDHT11 Object class to variable dht11
-const int buttonPin = 2;   //0 = fridge closed; 1 = fridge open
-const int pinDHT11int = 5;
+SimpleDHT11 dht11;              //instantiates the SimpleDHT11 Object class to variable dht11
+const int buttonPin = 2;        //0 = fridge closed; 1 = fridge open
+const int pinDHT11int = 5;      //front_view: signal, vcc, gnd
 const int pinDHT11ext = 6;
 const int BUZZER_PIN = 3;
 const int MAX_TEMP = 7;
