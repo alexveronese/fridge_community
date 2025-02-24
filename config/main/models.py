4861ff1111ebc9ce8e4e74b9d45e0c78211d4dc4
Fede Lopre <lpfederico40@gmail.com>
2025-02-24 11:42:55 +0100

    imp

--- config/main/models.py
+++ config/main/models.py
@@ -2,6 +2,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from django.db.models import ForeignKey
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 
